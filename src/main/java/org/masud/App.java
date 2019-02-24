a516eb72482e975f81818c5821659987eb2e1267
maliha hasan <masudrana87145@gmail.com>
2019-02-23 19:41:05 -0500

    My Java Commit

---
+++ src/main/java/org/masud/App.java
@@ -0,0 +1,13 @@
+package org.masud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
